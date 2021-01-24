40c9141592f26d6223b5c2f19e6f3ea0e14f117f
Nexus85 <mx38@mail.ru>
2021-01-24 15:42:21 +0300

    простая обратока

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from TOKEN import TOKEN
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 
 def main():
@@ -7,7 +7,20 @@ def main():
     Минимальное тело 
     """
     mybot = Updater(token=TOKEN)
+
+    #Диспечер
+    dp = mybot.dispatcher 
+
+    #Обработка старт
+    dp.add_handler(CommandHandler('start', greet_user))
+
+
+    
     mybot.start_polling()
     mybot.idle()
+
+def greet_user(bot, update):
+    print('Вызван /start')
+
 
 main()
