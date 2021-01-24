6af99faf8a34cea7e40d4f1a8de98d8c81510aec
Nexus85 <mx38@mail.ru>
2021-01-24 16:03:45 +0300

    добавил логирование

--- bot.py
+++ bot.py
@@ -2,16 +2,18 @@ from TOKEN import TOKEN
 from telegram.ext import Updater, CommandHandler
 import logging
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', lovel=logging.INFO, filename='bot.log')
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s - %(name)s', level=logging.INFO, filename='bot.log')
 
 def greet_user(bot, update):
     text = 'Вызван /start'
-    print(text)
+    logging.info(text)
     # print(dir(bot))
     bot.message.reply_text(text)
 
 
 def main():
+
+    logging.info('Бот запускается')
     """
     Минимальное тело 
     """
