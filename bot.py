de03c61ca94d873d72ae77d89a41f0b6f01c82d1
Nexus85 <mx38@mail.ru>
2021-01-24 15:52:30 +0300

    поправил функцию greet_user

--- bot.py
+++ bot.py
@@ -2,6 +2,13 @@ from TOKEN import TOKEN
 from telegram.ext import Updater, CommandHandler
 
 
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    # print(dir(bot))
+    bot.message.reply_text(text)
+
+
 def main():
     """
     Минимальное тело 
@@ -19,8 +26,7 @@ def main():
     mybot.start_polling()
     mybot.idle()
 
-def greet_user(bot, update):
-    print('Вызван /start')
+
 
 
 main()
