fed929cde3b4c19923f15d0e88dc72fe348f5716
Nexus85 <mx38@mail.ru>
2021-01-24 15:29:01 +0300

    простой бот, который ничего не делает

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
 from TOKEN import TOKEN
 from telegram.ext import Updater
 
+
+def main():
+    """
+    Минимальное тело 
+    """
+    mybot = Updater(token=TOKEN)
+    mybot.start_polling()
+    mybot.idle()
+
+main()
